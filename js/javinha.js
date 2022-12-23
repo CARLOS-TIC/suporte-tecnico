61e2c2fd5d9797adc4478732fe24e521e2b06d79
CARLOS CARVALHO <82110832+CARLOS-TIC@users.noreply.github.com>
2022-12-23 08:40:06 -0300

    cadastro de cnpj

--- js/javinha.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('#enviar')
-
-btn.addEventListener("click", function(event){
-    event.preventDefault()
-
-    const name = document.querySelector('#name'.value)
-
-    console.log
-})
